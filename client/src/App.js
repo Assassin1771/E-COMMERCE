d2917cd7c211bb542fa803327f16fbb52068fade
Tushar Garg <tshrgarg2010@gmail.com>
2021-11-29 21:34:31 +0530

    Created pages folder and did basic routing
    Created Login and Register inside auth subfolder of pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
 const App = () => {
   return (
-    <div>
-      <p>HELLO WORLD</p>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+    </Routes>
   );
 };
 
