79673f29febb55fea29dca5851fd346eed22409b
Tushar Garg <tshrgarg2010@gmail.com>
2021-11-29 21:55:11 +0530

    Styling and components folder
    Added Boostrap CSS and Ant Design. Also created Header component inside subfolder navbar of components

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,18 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import Header from "./components/nav/Header";
 
 const App = () => {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/login" element={<Login />} />
-      <Route exact path="/register" element={<Register />} />
-    </Routes>
+    <React.Fragment>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
+    </React.Fragment>
   );
 };
 
